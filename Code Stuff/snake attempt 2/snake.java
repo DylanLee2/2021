a8028dbe1c3997527ba1efd10a4f219aff591a73
DylanLee2 <56407983+DylanLee2@users.noreply.github.com>
2021-08-31 08:00:58 -0700

    8/31/21

--- Code Stuff/snake attempt 2/snake.java
+++ Code Stuff/snake attempt 2/snake.java
@@ -3,8 +3,10 @@ import java.util.ArrayList;
 public class snake{
 	
 	private int x, y, wid, hei;
-	private Rectangle apple;
+	private ArrayList<Rectangle> apple;
 	private ArrayList<Rectangle> body;
+	private boolean[] direction = new boolean[4];
+	private ArrayList<Integer> speedX, speedY;
 	
 	public snake(int startX, int startY, int width, int height){
 		x = startX;
@@ -13,40 +15,64 @@ public class snake{
 		hei = height;
 		body = new ArrayList<Rectangle>();
 		body.add(new Rectangle(x, y, width, height));
-		apple = new Rectangle((int)(Math.random()*500), (int)(Math.random()*500), wid, hei);
-		apple.setColor(Color.RED);
-		apple.fill();
+		color(0);
+		apple = new ArrayList<Rectangle>();
+		apple.add(new Rectangle((int)(Math.random()*500), (int)(Math.random()*500), wid, hei));
+		apple.get(0).setColor(Color.RED);
+		apple.get(0).draw();
+		speedX = new ArrayList<Integer>();
+		speedY = new ArrayList<Integer>();
 	}
 	
-	public void grow(String direction){
-		if(body.get(0).contains(apple)){
+	public void addSpeed(int xSpeed, int ySpeed){
+		speedX.add(xSpeed);
+		speedY.add(ySpeed);
+	}
+	
+	public void grow(boolean up, boolean down, boolean left, boolean right){
+		if(body.get(0).contains(apple.get(apple.size()-1))){
+			apple.get(apple.size()-1).translate(600, 0);
+			apple.set(apple.size()-1, null);
 			System.out.println("hit");
-			if(direction.equals("w"))
+			if(down == false && up == true){
+				System.out.println("up");
 				body.add(new Rectangle(body.get(body.size()-1).getX(), body.get(body.size()-1).getY() + hei,  wid, hei));
-			else if(direction.equals("a"))
+			}
+			else if(left == true && right == false){
+				System.out.println("left");
 				body.add(new Rectangle(body.get(body.size()-1).getX() + wid, body.get(body.size()-1).getY(),  wid, hei));
-			else if(direction.equals("s"))
+			}
+			else if(down == true && up == false){
+				System.out.println("down");
 				body.add(new Rectangle(body.get(body.size()-1).getX(), body.get(body.size()-1).getY() - hei,  wid, hei));
-			else if(direction.equals("d"))
+			}
+			else if(right == true && left == false){
+				System.out.println("right");
 				body.add(new Rectangle(body.get(body.size()-1).getX() - wid, body.get(body.size()-1).getY(),  wid, hei));
+			}
 			color(body.size()-1);
-			apple = new Rectangle((int)(Math.random()*500), (int)(Math.random()*500), wid, hei);
+			
+			apple.add(new Rectangle((int)(Math.random()*500), (int)(Math.random()*500), wid, hei));
+			apple.get(apple.size()-1).setColor(Color.RED);
+			apple.get(apple.size()-1).draw();
 		}
 	}
 	
 	public void move(int moveX, int moveY){
 		for(int i = 0; i < body.size(); i++){
+			//body.get(i).translate(moveX, moveY);
+			
 			if(i == 0)
 				body.get(0).translate(moveX, moveY);
-			else
-				setBody(body.get(i), body.get(i-1));
+			//else
+				//body.get(i).translate(speedX.get(speedX.size()-i), speedY.get(speedY.size()-i));
+			
 		}
 	}
 	
 	//change position of next box to current box
 	public void setBody(Rectangle one, Rectangle two){
 		one = new Rectangle(two.getX(), two.getY(), wid, hei);
-		color();
 	}
 	
 	public void color(int i) {
--- Code Stuff/snake attempt 2/starter.java
+++ Code Stuff/snake attempt 2/starter.java
@@ -1,43 +1,58 @@
 import pkg.*;
-
 public class starter implements InputKeyControl {
 	
-	public static String inp;
-	public static int speedX, speedY, moveX, moveY, speed = 2;
+	public static int moveX, moveY, speed = 2;
 	public static snake sna;
-	public static boolean game = true;
+	public static boolean[] direction = new boolean[4]; // 0=w, 1=a, 2=s, 3=d
 	
 	public static void main(String args[]){
       KeyController kC = new KeyController(Canvas.getInstance(),new starter());
 		
+		for(int i = 0; i < direction.length; i++){
+			direction[i] = true;
+		}
 		sna = new snake(250, 250, 30, 30);
-		sna.color(0);
 		while(true){
 			Canvas.pause(10);
 			sna.move(moveX, moveY);
-			sna.grow(inp);
+			sna.grow(direction[0], direction[1], direction[2], direction[3]);
 		}
     }
     
 	public void keyPress(String s){
-		inp = s;
-		if(inp.equals("w")){
+		//moveX = inp.equals("w") ? 0 : inp.equals("a") ? -1*speed : inp.equals("s") ? 0 : inp.equals("d") ? speed : moveX;
+		//moveY = inp.equals("w") ? -1*speed : inp.equals("a") ? 0 : inp.equals("s") ? speed : inp.equals("d") ? 0 : moveY;
+		
+		if(s.equals("w") && direction[0] == true){
 			moveX = 0;
 			moveY = -1*speed;
+			direction[2] = false;
+			for(int i = 0; i < direction.length; i++)
+				direction[i] = i!=2 ? true : false;
 		}
-		else if(s.equals("a")){
+		else if(s.equals("a") && direction[1] == true){
 			moveX = -1*speed;
 			moveY = 0;
+			direction[3] = false;
+			for(int i = 0; i < direction.length; i++)
+				direction[i] = i!=3 ? true : false;
 		}
-		else if(s.equals("s")){
+		else if(s.equals("s") && direction[2] == true){
 			moveX = 0;
 			moveY = speed;
+			direction[0] = false;
+			for(int i = 0; i < direction.length; i++)
+				direction[i] = i!=0 ? true : false;
 		}
-		else if(s.equals("d")){
+		else if(s.equals("d") && direction[3] == true){
 			moveX = speed;
 			moveY = 0;
+			direction[1] = false;
+			for(int i = 0; i < direction.length; i++)
+				direction[i] = i!=1 ? true : false;
 		}
+		sna.addSpeed(moveX, moveY);
+		
 	}
 	
-
 }
