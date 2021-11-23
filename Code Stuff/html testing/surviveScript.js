4af734c70d14fb04c913dc457ed6f46f08b2af8e
Dylan Lee <56407983+DylanLee2@users.noreply.github.com>
2021-11-23 08:16:18 -0800

    11/23/21

--- Code Stuff/html testing/surviveScript.js
+++ Code Stuff/html testing/surviveScript.js
@@ -56,7 +56,10 @@ class Player{
     this.y = y;
     this.w = w;
     this.h = h;
-    this.speed = 0;
+    this.hp = 3;
+    this.maxSpeed = 4;
+    this.speedX = 0;
+    this.speedY = 0;
     this.image = document.getElementById("player");
   }
 
@@ -65,45 +68,91 @@ class Player{
   }
   
   move(moveX, moveY){
-    c.clearRect(0,0,600,600);
     this.x = this.x + moveX;
     this.y = this.y + moveY;
     c.drawImage(this.image,this.x,this.y,this.w,this.h);
   }
 
   updatePosition(){
-    c.clearRect(0,0,600,600);
-    this.y += this.speed;
+    this.x += this.speedX;
+    this.y += this.speedY;
     c.drawImage(this.image,this.x,this.y,this.w,this.h);
   }
   
 }
 
+class Projectile{
+  constructor(x,y,w,h,color){
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.color = color;
+  }
+
+  draw(){
+    c.fillStyle = this.color;
+    c.fillRect(this.x,this.y,this.w,this.h);
+  }
+
+  updatePosition(){
+    this.x += this.speedX;
+    this.y += this.speedY;
+    c.fillRect(this.x,this.y,this.w,this.h);
+  }
+
+}
+/*
+class Enemy{
+  constructor(x,y,w,h){
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+  }
+}
+*/
 document.onkeydown = keyPress;
 document.onkeyup = stop;
-var p = new Player(100,100,70,65);
+document.onclick = shoot;
+var p = new Player(250,250,70,65);
 p.draw();
+var bullet = new Projectile(-10,-10,10,10,"blue");
+bullet.draw();
+
+function shoot(e){
+  let ex = e.pageX-205;
+  let why = e.pageY-120;
+  bullet = new Projectile(ex,why,10,10,"blue");
+}
 
 function stop(){
-  p.speed = 0;
+  p.speedX = 0;
+  p.speedY = 0;
 }
 
 function keyPress(e){
   let key = e.keyCode;
   //w = 87, up arrow = 38
   //s = 83, down arrow = 40
-  if(key == 87 || key == 38){
-    p.speed = -2; 
-    //p.move(0,-10);
-  }
-  else if(key == 83 || key == 40){
-    p.speed = 2;
-    //p.move(0,10);
-  }
+  if(key == 87 || key == 38)
+    p.speedY = -p.maxSpeed;
+  else if(key == 83 || key == 40)
+    p.speedY = p.maxSpeed;
+  //a = 65, left arrow = 37
+  //d = 68, right arrow = 39
+  if(key == 65 || key == 37)
+    p.speedX = -p.maxSpeed; 
+  else if(key ==  68|| key == 39)
+    p.speedX = p.maxSpeed;
 }
 
 function update(){
+  c.clearRect(0,0,600,600);
   p.updatePosition();
+  bullet.updatePosition();
   if(p.x > 600)
     p.x = -50;
   else if(p.x < -50)
@@ -113,4 +162,10 @@ function update(){
   else if(p.y < -50)
     p.y = 600;
 }
+/*
+addEventListener("click", (event)=>{
+  bullet = new Projectile(event.clientX, event.clientY, 10,10, "red")
+  bullet.draw();
+})
+*/
 setInterval(update,20); // basically canvas.pause
